f459778171c89feaa1feb8a1a5913edc642720e4
hchiam <hchiam@users.noreply.github.com>
2021-05-09 23:18:08 -0400

    stub for export/import reqs notes, maybe as excel/jspreadsheet (#14 and #15)

--- index.js
+++ index.js
@@ -51,6 +51,18 @@ function attachEventListeners() {
   $("#export_html_file").on("click", function () {
     saveHtmlFile($("#output_html_string pre").text());
   });
+
+  $("#export_requirements").on("click", function () {
+    alert(
+      'This button "Export requirements" will be worked on if requested. \n\nMaybe an excel sheet will be exported. \n\nMaybe we can use jspreadsheet, which works like excel in your browser. \n\n'
+    );
+  });
+
+  $("#import_requirements").on("click", function () {
+    alert(
+      'This button "Import requirements" will be worked on if requested. \n\nMaybe an excel sheet can be imported. \n\nOr maybe we can let users import from excel into jspreadsheet in the browser. \n\n'
+    );
+  });
 }
 
 function deleteTemplateInstance(button) {
