19ddfac26597c02f390ecf03d279633dadd08697
hchiam <hchiam@users.noreply.github.com>
2021-05-23 15:11:50 -0400

    clean up jSpreadsheet context menu

--- index.js
+++ index.js
@@ -538,6 +538,7 @@ function setUpJSpreadsheet() {
   let spreadsheet = jspreadsheet(document.getElementById("spreadsheet"), {
     data: JSON.parse(JSON.stringify(defaultData)),
     columns: JSON.parse(JSON.stringify(columnDefinitions)),
+    contextMenu: setUpJSpreadsheetContextMenu,
   });
 
   $("#export_sheet").on("click", function () {
@@ -570,6 +571,129 @@ function setUpJSpreadsheet() {
   spreadsheet.getHeaders = () => oldGetHeaders().split(",");
   spreadsheet.getRows = spreadsheet.getData;
   return spreadsheet;
+}
+
+function setUpJSpreadsheetContextMenu(obj, x, y, e) {
+  const items = [];
+
+  if (y == null) {
+    if (obj.options.allowInsertColumn == true) {
+      items.push({
+        title: obj.options.text.insertANewColumnBefore,
+        onclick: function () {
+          obj.insertColumn(1, parseInt(x), 1);
+        },
+      });
+      items.push({
+        title: obj.options.text.insertANewColumnAfter,
+        onclick: function () {
+          obj.insertColumn(1, parseInt(x), 0);
+        },
+      });
+    }
+
+    if (obj.options.allowDeleteColumn == true) {
+      items.push({
+        title: obj.options.text.deleteSelectedColumns,
+        onclick: function () {
+          obj.deleteColumn(
+            obj.getSelectedColumns().length ? undefined : parseInt(x)
+          );
+        },
+      });
+    }
+
+    if (obj.options.allowRenameColumn == true) {
+      items.push({
+        title: obj.options.text.renameThisColumn,
+        onclick: function () {
+          obj.setHeader(x);
+        },
+      });
+    }
+
+    if (obj.options.columnSorting == true) {
+      items.push({ type: "line" });
+
+      items.push({
+        title: obj.options.text.orderAscending,
+        onclick: function () {
+          obj.orderBy(x, 0);
+        },
+      });
+      items.push({
+        title: obj.options.text.orderDescending,
+        onclick: function () {
+          obj.orderBy(x, 1);
+        },
+      });
+    }
+  } else {
+    if (obj.options.allowInsertRow == true) {
+      items.push({
+        title: obj.options.text.insertANewRowBefore,
+        onclick: function () {
+          obj.insertRow(1, parseInt(y), 1);
+        },
+      });
+
+      items.push({
+        title: obj.options.text.insertANewRowAfter,
+        onclick: function () {
+          obj.insertRow(1, parseInt(y));
+        },
+      });
+    }
+
+    if (obj.options.allowDeleteRow == true) {
+      items.push({
+        title: obj.options.text.deleteSelectedRows,
+        onclick: function () {
+          obj.deleteRow(obj.getSelectedRows().length ? undefined : parseInt(y));
+        },
+      });
+    }
+
+    if (x) {
+      if (obj.options.allowComments == true) {
+        items.push({ type: "line" });
+
+        var title = obj.records[y][x].getAttribute("title") || "";
+
+        items.push({
+          title: title
+            ? obj.options.text.editComments
+            : obj.options.text.addComments,
+          onclick: function () {
+            obj.setComments([x, y], prompt(obj.options.text.comments, title));
+          },
+        });
+
+        if (title) {
+          items.push({
+            title: obj.options.text.clearComments,
+            onclick: function () {
+              obj.setComments([x, y], "");
+            },
+          });
+        }
+      }
+    }
+  }
+
+  items.push({ type: "line" });
+
+  if (obj.options.allowExport) {
+    items.push({
+      title: obj.options.text.saveAs,
+      shortcut: "Ctrl + S",
+      onclick: function () {
+        obj.download();
+      },
+    });
+  }
+
+  return items;
 }
 
 function generateHtmlFromSheet(headersArray, dataRows) {
