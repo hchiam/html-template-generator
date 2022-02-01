37334a4c24250c2e5f6188bb89815703417d3294
hchiam <hchiam@users.noreply.github.com>
2022-02-01 00:38:29 -0500

    replace template_demo.gif string for CodePen demo only

--- concat_for_codepen.sh
+++ concat_for_codepen.sh
@@ -4,6 +4,7 @@ cat codepen_only.css styles.css > ./CodePen/CSS.css
 # HTML:
 html=$(sed -n '/    <!-- copy to https:\/\/codepen.io\/hchiam\/pen\/jOBOaqm starting here -->/,/    <!-- copy to https:\/\/codepen.io\/hchiam\/pen\/jOBOaqm ending here -->/p' index.html | sed '1d;$d');
 echo "$html" > ./CodePen/HTML.html
+sed -i '' 's/template_demo.gif/https:\/\/raw.githubusercontent.com\/hchiam\/html-template-generator\/main\/template_demo.gif/g' ./CodePen/HTML.html
 
 # JS:
 cat codepen_only.js index.js helpers/*.js > ./CodePen/JS.js
