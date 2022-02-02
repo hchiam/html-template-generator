e7b8204c4fa9faeb09c5acdf6dd341297737e80d
hchiam <hchiam@users.noreply.github.com>
2022-02-01 21:50:41 -0500

    fix #39: copy notes as well when copy template

--- index.js
+++ index.js
@@ -333,6 +333,11 @@ function copyTemplate(button, extraData) {
     isExample ? lastTemplateInOutputContainer : templateContainer
   ).next();
 
+  const note = templateContainer.find(".notes").val();
+  if (note) {
+    destinationElement.find(".notes").val(note);
+  }
+
   destinationElement.css("visibility", "hidden");
 
   const allNextElements = destinationElement.next(
