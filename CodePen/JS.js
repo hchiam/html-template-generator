86e03cf1709992f9e649a1e209ed673430441ae7
Howard Chiam <hchiam@users.noreply.github.com>
2022-02-04 22:41:17 -0500

    fix #68: get rid of the animation hiding grey borders due to too many usability issues (#69)

--- CodePen/JS.js
+++ CodePen/JS.js
@@ -597,7 +597,6 @@ function saveHtmlFile(html) {
 }
 
 function animateMove(originJQueryElement, destinationJQueryElement) {
-  setTimeout(() => {
     destinationJQueryElement.css("visibility", "hidden");
     const original = $(originJQueryElement);
     const originalMarginLeft = parseInt(original.css("marginLeft"));
@@ -633,7 +632,6 @@ function animateMove(originJQueryElement, destinationJQueryElement) {
       $(destinationJQueryElement).css("visibility", "visible");
       $("#output").removeAttr("data-animating", "");
     }, 1000);
-  }, 300);
 }
 
 function setUpJSpreadsheet() {
